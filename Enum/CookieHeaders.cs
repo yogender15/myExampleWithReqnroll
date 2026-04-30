f5d2f972a1787b8a53f35099530f64b1d63606c3
Yogender Prakash <yogendermp15@outlook.com>
2026-04-30 15:40:42 +0100

    Add files via upload

---
+++ Enum/CookieHeaders.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace POMSeleniumFrameworkPoc1.Enum
+{
+    public enum CookieHeaders
+    {
+        CrmOwinAuth,
+        ReqClientId,
+        OrgId
+    }
+}
