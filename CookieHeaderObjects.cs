270e7b236fd2666bb6290d45a92de908a16fdb80
Yogender Prakash <yogendermp15@outlook.com>
2026-04-30 15:35:48 +0100

    Add files via upload

---
+++ CookieHeaderObjects.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace POMSeleniumFrameworkPoc1.APIObjects
+{
+   public class CookieHeaderObjects
+    {
+        public string ReqClientId { get; set; }
+        public string OrgId { get; set; }
+        public string CrmOwinAuth { get; set; }
+    }
+}
---
+++ SearchJobRequestObjects.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace POMSeleniumFrameworkPoc1.APIObjects
+{
+   public class SearchJobRequestObjects
+    {
+        public string Search { get; set; }
+        public int Top { get; set; }
+        public bool UseFuzzy { get; set; }
+        public string SuggestType { get; set; }
+    }
+}
