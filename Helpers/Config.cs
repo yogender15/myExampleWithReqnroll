1265ac78fb2690767203f5865ecdc53ab90829d0
Yogender Prakash <yogendermp15@outlook.com>
2026-04-30 15:20:51 +0000

    Refactor Config class for cleaner syntax

--- Helpers/Config.cs
+++ Helpers/Config.cs
@@ -1,13 +1,9 @@
-﻿using System;
+using System;
 using System.Configuration;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using NUnit.Framework;
-
-[assembly: Parallelizable(ParallelScope.Fixtures)]
-[assembly: LevelOfParallelism(2)]
 
 namespace POMSeleniumFrameworkPoc1.Helpers
 {
