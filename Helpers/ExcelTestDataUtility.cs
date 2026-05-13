20563a9b06e85d73694aee7bcb83d24c98489868
yogender15 <yogendermp15@outlook.com>
2026-05-13 09:10:06 +0100

    ExcelTestDataUtility cleaned up

--- Helpers/ExcelTestDataUtility.cs
+++ Helpers/ExcelTestDataUtility.cs
@@ -1,10 +1,8 @@
-﻿using OfficeOpenXml;
+using OfficeOpenXml;
 using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace POMSeleniumFrameworkPoc1.Helpers
 {
@@ -19,25 +17,26 @@ namespace POMSeleniumFrameworkPoc1.Helpers
 
         public Dictionary<string, string> GetTestDataByID(string sheetName, string testCaseID)
         {
-
             var testData = new Dictionary<string, string>();
 
             using (var package = new ExcelPackage(new FileInfo(filePath)))
             {
                 var worksheet = package.Workbook.Worksheets[sheetName];
                 var headers = GetHeaders(worksheet);
-                var row = FindRowByColumnValue(worksheet, "TestDataID", testCaseID);
+                var row = FindRowByColumnValue(worksheet, headers, "TestDataID", testCaseID);
 
-                foreach (var header in headers)
+                // Bug fix: use loop index directly instead of IndexOf
+                // IndexOf always returns the first occurrence — wrong column if duplicates exist
+                for (int colIndex = 0; colIndex < headers.Count; colIndex++)
                 {
-                    var columnIndex = headers.IndexOf(header) + 1;
+                    var header = headers[colIndex];
+                    var columnIndex = colIndex + 1;
                     var cellValue = worksheet.Cells[row, columnIndex].Value?.ToString();
                     try
                     {
                         testData.Add(header, cellValue);
-                        //Console.WriteLine(header + " : "+ cellValue);
                     }
-                    catch (Exception e)
+                    catch (Exception)
                     {
                         throw new Exception($"Exception while adding {header}");
                     }
@@ -54,51 +53,54 @@ namespace POMSeleniumFrameworkPoc1.Helpers
             for (int i = 1; i <= columnCount; i++)
             {
                 headers.Add(worksheet.Cells[1, i].Value?.ToString());
-
             }
             return headers;
         }
 
-        private int FindRowByColumnValue(ExcelWorksheet worksheet, string columnName, string value)
+        // Bug fix: accept pre-read headers so GetHeaders is not called on every row
+        // Bug fix: Trim() both sides so trailing/leading spaces in Excel cells do not break the match
+        private int FindRowByColumnValue(ExcelWorksheet worksheet, List<string> headers, string columnName, string value)
         {
+            var columnIndex = GetColumnIndex(headers, columnName);
             var rowCount = worksheet.Dimension.End.Row;
+
             for (int i = 2; i <= rowCount; i++)
             {
-                var cellValue = worksheet.Cells[i, GetColumnIndex(worksheet, columnName)].Value?.ToString();
-                if (cellValue == value)
+                var cellValue = worksheet.Cells[i, columnIndex].Value?.ToString()?.Trim();
+                if (cellValue == value?.Trim())
                 {
                     return i;
                 }
-
             }
             throw new InvalidOperationException($"No row found with {columnName} = {value}");
         }
 
-        private int GetColumnIndex(ExcelWorksheet worksheet, string columnName)
+        // Bug fix: accepts headers list instead of re-reading from Excel on every call
+        private int GetColumnIndex(List<string> headers, string columnName)
         {
-            var headers = GetHeaders(worksheet);
             return headers.IndexOf(columnName) + 1;
         }
 
-
-        public String getFieldTestData(String fieldName, string sheetName, string testCaseID)
+        public string getFieldTestData(string fieldName, string sheetName, string testCaseID)
         {
-            var testData = new Dictionary<string, string>();
             using (var package = new ExcelPackage(new FileInfo(filePath)))
             {
                 var worksheet = package.Workbook.Worksheets[sheetName];
                 var headers = GetHeaders(worksheet);
-                var row = FindRowByColumnValue(worksheet, "TestDataID", testCaseID);
+                var row = FindRowByColumnValue(worksheet, headers, "TestDataID", testCaseID);
+                var testData = new Dictionary<string, string>();
 
-                foreach (var header in headers)
+                // Bug fix: use loop index directly instead of IndexOf
+                for (int colIndex = 0; colIndex < headers.Count; colIndex++)
                 {
-                    var columnIndex = headers.IndexOf(header) + 1;
+                    var header = headers[colIndex];
+                    var columnIndex = colIndex + 1;
                     var cellValue = worksheet.Cells[row, columnIndex].Value?.ToString();
                     testData.Add(header, cellValue);
                 }
+
+                return testData[fieldName];
             }
-            String fieldValue = testData[fieldName];
-            return testData[fieldName];
         }
     }
 }
