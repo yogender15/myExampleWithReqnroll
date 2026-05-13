9502d89162999696706289254a679968781643b3
yogender15 <yogendermp15@outlook.com>
2026-05-13 14:56:42 +0100

    Hooks Code Cleanup

--- Helpers/Hooks.cs
+++ Helpers/Hooks.cs
@@ -1,11 +1,7 @@
-using AventStack.ExtentReports;
-using AventStack.ExtentReports.Gherkin.Model;
-using AventStack.ExtentReports.Model;
-using AventStack.ExtentReports.Reporter;
-using AventStack.ExtentReports.Reporter.Config;
 using Serilog;
 using System;
 using System.IO;
+using System.Linq;
 using Reqnroll;
 using Reqnroll.Infrastructure;
 using Log = Serilog.Log;
@@ -16,11 +12,6 @@ namespace POMSeleniumFrameworkPoc1.Helpers
     [Binding]
     public class Hooks
     {
-        public static ExtentReports extent = new ExtentReports();
-        private static ExtentTest extentTest;
-        private static ExtentTest scenarioTest;
-        public static string testResultsRootPath;
-
         private readonly IReqnrollOutputHelper _specFlowOutputHelper;
         public ScenarioContext _scenarioContext;
         public FeatureContext _featureContext;
@@ -39,14 +30,8 @@ namespace POMSeleniumFrameworkPoc1.Helpers
                 .WriteTo.Console(outputTemplate: "[{Timestamp:HH:mm:ss} {Level:u3}] {Message:lj}{NewLine}{Exception}")
                 .CreateLogger();
 
-            testResultsRootPath = AppDomain.CurrentDomain.BaseDirectory + @"..\..\TestResults";
-            Directory.CreateDirectory(testResultsRootPath);
-
-            var sparkReporter = new ExtentSparkReporter(Path.Combine(testResultsRootPath, "Report.html"));
-            sparkReporter.Config.ReportName = "Automation Status Report";
-            sparkReporter.Config.DocumentTitle = "Automation Status Report";
-            sparkReporter.Config.Theme = Theme.Dark;
-            extent.AttachReporter(sparkReporter);
+            Directory.CreateDirectory(
+                AppDomain.CurrentDomain.BaseDirectory + @"..\..\TestResults");
 
             SetBaseUrl();
         }
@@ -54,28 +39,34 @@ namespace POMSeleniumFrameworkPoc1.Helpers
         [BeforeFeature]
         public static void BeforeFeature(FeatureContext context)
         {
-            Log.Information("Selecting feature File {0} to run", context.FeatureInfo.Title);
-            extentTest = extent.CreateTest<Feature>(context.FeatureInfo.Title.ToString());
+            Log.Information("Selecting feature file: {Feature}", context.FeatureInfo.Title);
         }
 
         [BeforeScenario("UI")]
         public void BeforeScenario()
         {
             DriverHelper.Driver = DriverHelper.DriverInitiation();
-            scenarioTest = extentTest.CreateNode<Scenario>(_scenarioContext.ScenarioInfo.Title);
+
+            var title = _scenarioContext.ScenarioInfo.Title;
+            var safeName = new string(title.Take(60).ToArray())
+                .Replace(":", "").Replace("/", "").Replace("\\", "").Replace("\"", "");
+            var pdf_Util = new PDF_Utility();
+            pdf_Util.initializeScreenshotsFile(safeName);
         }
 
         [AfterScenario("UI")]
         public void AfterScenario()
         {
+            var pdf_Util = new PDF_Utility();
+            pdf_Util.finalizeScreenshotsFile();
+
             DriverHelper.DriverDispose();
-            Log.Information("The driver has been disposed");
+            Log.Information("Driver disposed");
         }
 
         [AfterTestRun]
         public static void AfterTestRun()
         {
-            extent.Flush();
             Log.CloseAndFlush();
         }
 
@@ -86,52 +77,13 @@ namespace POMSeleniumFrameworkPoc1.Helpers
         }
 
         [AfterStep]
-        public static void AfterStep()
+        public void AfterStep()
         {
-            var scenarioInfo = ScenarioContext.Current;
-            var stepType = ScenarioStepContext.Current.StepInfo.StepDefinitionType.ToString();
+            var pdf_Util = new PDF_Utility();
+            pdf_Util.takeScreenshot();
 
-            if (scenarioInfo.TestError == null)
-            {
-                switch (stepType)
-                {
-                    case "Given":
-                        scenarioTest.CreateNode<Given>(ScenarioStepContext.Current.StepInfo.Text);
-                        break;
-                    case "When":
-                        scenarioTest.CreateNode<When>(ScenarioStepContext.Current.StepInfo.Text);
-                        break;
-                    case "Then":
-                        scenarioTest.CreateNode<Then>(ScenarioStepContext.Current.StepInfo.Text);
-                        break;
-                    case "And":
-                        scenarioTest.CreateNode<And>(ScenarioStepContext.Current.StepInfo.Text);
-                        break;
-                }
-            }
-            else
-            {
-                var screenshotPath = DriverHelper.TakeScreenShot();
-                switch (stepType)
-                {
-                    case "Given":
-                        scenarioTest.CreateNode<Given>(ScenarioStepContext.Current.StepInfo.Text)
-                            .Fail(scenarioInfo.TestError.Message, MediaEntityBuilder.CreateScreenCaptureFromPath(screenshotPath).Build());
-                        break;
-                    case "When":
-                        scenarioTest.CreateNode<When>(ScenarioStepContext.Current.StepInfo.Text)
-                            .Fail(scenarioInfo.TestError.Message, MediaEntityBuilder.CreateScreenCaptureFromPath(screenshotPath).Build());
-                        break;
-                    case "Then":
-                        scenarioTest.CreateNode<Then>(ScenarioStepContext.Current.StepInfo.Text)
-                            .Fail(scenarioInfo.TestError.Message, MediaEntityBuilder.CreateScreenCaptureFromPath(screenshotPath).Build());
-                        break;
-                    case "And":
-                        scenarioTest.CreateNode<And>(ScenarioStepContext.Current.StepInfo.Text)
-                            .Fail(scenarioInfo.TestError.Message, MediaEntityBuilder.CreateScreenCaptureFromPath(screenshotPath).Build());
-                        break;
-                }
-            }
+            if (_scenarioContext.TestError != null)
+                pdf_Util.loggerInNewPage(_scenarioContext.TestError.ToString());
         }
 
         private static void SetBaseUrl()
